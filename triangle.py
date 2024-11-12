1c0950c3ecfecca5fc06321119f323bf184d73b3
Arun kumar <68185675+ARUNexe@users.noreply.github.com>
2024-11-12 21:10:29 +0530

    Update the code

--- triangle.py
+++ triangle.py
@@ -1,51 +1,37 @@
+
 import pygame
 import random
 import time
-import math
 
 pygame.init()
 
-screen = pygame.display.set_mode((500, 500))
+screen = pygame.display.set_mode((1100, 1000))
+screen.fill((255, 255, 255))
 
-def midpt(mp_x,mp_y,*xy):
-    x = xy[0][0][0]
-    y = xy[0][0][1]
-    x_mid_pt = (mp_x+x)/2
-    y_mid_pt = (mp_y+y)/2
+
+def calculate_midpoint(node,vertice):
+    x = vertice[0][0]
+    y = vertice[0][1]
+    x_mid_pt = (node[0]+x)/2
+    y_mid_pt = (node[1]+y)/2
     return (x_mid_pt,y_mid_pt)
 
-pt1 = (250,100)
-pt2 = (100,400)
-pt3 = (400,400)
-screen.fill((255, 255, 255))
-pts = [(250,100),(100,400),(400,400)]
+pt1 = (550,100)
+pt2 = (100,900)
+pt3 = (1000,900)
+vertices = [pt1,pt2,pt3]
 
-start_lst = random.sample(pts,2)
-print(start_lst)
+node = pt1
 
-mid_pt_x = (start_lst[0][0]+start_lst[1][0])/2
-mid_pt_y = (start_lst[0][1]+start_lst[1][1])/2
+while True:
+    for i in vertices:
+        pygame.draw.circle(screen, (0, 0, 0), i, 4)
 
-runner = True
-while runner:
-    for event in pygame.event.get():
-        if (event.type == pygame.QUIT):
-            runner = False
+    random_vertices = random.sample(vertices,1)
+    midpoint = calculate_midpoint(node,random_vertices)
 
-    pygame.draw.circle(screen, (0, 0, 0), pt1, 1)
-    pygame.draw.circle(screen, (0, 0, 0), pt2, 1)
-    pygame.draw.circle(screen, (0, 0, 0), pt3, 1)
+    node = midpoint
 
-    pygame.draw.circle(screen, (0, 0, 0), (mid_pt_x, mid_pt_y), 1)
-
-    out_pt = random.sample(pts,1)
-
-    new_md = midpt(mid_pt_x,mid_pt_y,out_pt)
-
-    pygame.draw.circle(screen, (0, 0, 0), (new_md[0],new_md[1]), 1)
-    #time.sleep(0.0005)
-    print(new_md[0],new_md[1])
-    mid_pt_x = new_md[0]
-    mid_pt_y = new_md[1]
+    pygame.draw.circle(screen, (0, 0, 0), node, 1)
 
     pygame.display.update()
